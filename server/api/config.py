e96a17ef197f22d36589a5bf63274c98150ec1f8
Manik Rana <manikrana54@gmail.com>
2023-02-19 22:05:21 +0530

    Update to support planetscale

--- server/api/config.py
+++ server/api/config.py
@@ -1,8 +1,1 @@
-import os
-
-APP_ENV = os.getenv("APP_ENV", "development")
-DATABASE_USERNAME = os.getenv("DATABASE_USERNAME", "root")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "password")
-DATABASE_HOST = os.getenv("DATABASE_HOST", "127.0.0.1")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "postman")
-DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
+DATABASE_URL = 'mysql://sa273rvop01ts4lxg5xq:<PASSWORD>@ap-south.connect.psdb.cloud/postman-workshop?ssl={"rejectUnauthorized":true}'
